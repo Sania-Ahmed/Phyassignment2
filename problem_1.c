2c69e778570bb38844629f8bb748ba3b2a4d2bde
Sania-Ahmed <tasminsania77@gmail.co>
2025-12-04 16:53:30 +0600

    problem one done

--- problem_1.c
+++ problem_1.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
-    printf("initializziinnnnggSS");
+    /**
+      Finding the M-th order maxima using the formula: 
+      M = dsinθ / λ
+    **/
+
+    double wl , angle, d; 
+
+    printf("Please enter the wavelength , angle and distance between slits: ");
+
+    scanf("%lf %lf %lf", &wl , &angle, &d); 
+
+    if (wl < 380 || wl > 750) {
+
+        printf("Out of the range. Please enter a valid number.\n");
+    }
+    else{
+         double wl2 = wl/1000000000;  
+
+         double d2  = d/1000000; 
+
+        double angle2 = angle*(3.1416 / 180);
+
+        int M = (d2 * sin(angle2)) / wl2;
+
+        printf ("M-th order maxima: %d\n", M); 
+    }
+    
+
     return 0;
 }
